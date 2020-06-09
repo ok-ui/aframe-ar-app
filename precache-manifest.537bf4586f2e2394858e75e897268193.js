97e230ddde29f6a6e717e10ad844d01bcf62f82d
ovarunendra <vpsingh016@gmail.com>
2020-06-09 19:39:18 +0530

    Updates

---
+++ precache-manifest.537bf4586f2e2394858e75e897268193.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39bba6bf370fbfdd206b42dee63e93db",
+    "url": "/aframe-ar-app/index.html"
+  },
+  {
+    "revision": "dd03ddba7fcd78981c41",
+    "url": "/aframe-ar-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0dabfa5b41ea7272b495",
+    "url": "/aframe-ar-app/static/js/2.cc3939e0.chunk.js"
+  },
+  {
+    "revision": "763ff92ed9c23c83eb78d5f41890f2a7",
+    "url": "/aframe-ar-app/static/js/2.cc3939e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd03ddba7fcd78981c41",
+    "url": "/aframe-ar-app/static/js/main.5d493c56.chunk.js"
+  },
+  {
+    "revision": "bccf29d32e39eaf01699",
+    "url": "/aframe-ar-app/static/js/runtime-main.7b430031.js"
+  }
+]);
