b3e1de5fa919f0bcbfeeb8c8931f5b7e1d1c688c
ovarunendra <vpsingh016@gmail.com>
2020-06-09 19:51:02 +0530

    box

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ function App() {
   return (
     <div className="App">
       <a-scene>
+        <a-assets>
+          <img
+            id="boxTexture"
+            alt="boxTexture"
+            src="https://i.imgur.com/mYmmbrp.jpg"
+          />
+        </a-assets>
+
         <a-box
-          color="red"
+          src="#boxTexture"
           position="0 2 -5"
           rotation="0 45 45"
           scale="2 2 2"
+          animation="property: object3D.position.y; to: 2.2; dir: alternate; dur: 2000; loop: true"
         ></a-box>
+
+        <a-camera>
+          <a-cursor></a-cursor>
+        </a-camera>
       </a-scene>
     </div>
   );
